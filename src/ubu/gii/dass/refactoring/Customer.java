5b4d50172038bdb4ed42300d2db7fd1ab5e7d92a
RobertoMiranda <rmp0046@gmail.com>
2015-05-14 18:23:42 +0200

    #1 Rename local variable 'each' in 'ubu.gii.dass.refactoring.Customer.getFrequentRenterPoints(...)' to 'rental' - Original project: 'refactoring-fowler-example' - Original element: 'ubu.gii.dass.refactoring.Customer.getFrequentRenterPoints(Rental).each' - Renamed element: 'each' - Update references to refactored element
    Code error fix for the test.

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -41,7 +41,7 @@ public class Customer {
 			// determine amounts for each line
 			thisAmount = each.getCharge();
 			
-			frequentRenterPoints = getFrequentRenterPoints(
+			frequentRenterPoints += getFrequentRenterPoints(
 					each);
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
@@ -55,13 +55,13 @@ public class Customer {
 		return result;
 	}
 
-	private int getFrequentRenterPoints(Rental each) {
+	private int getFrequentRenterPoints(Rental aRental) {
 		int frequentRenterPoints=0;
 		// add frequent renter points
 		frequentRenterPoints++;
 		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& each.getDaysRented() > 1)
+		if ((aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& aRental.getDaysRented() > 1)
 			frequentRenterPoints++;
 		return frequentRenterPoints;
 	}
